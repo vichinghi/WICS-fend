59388b8151fe1c0c4342ef2c718b451c4116c24f
kenneth051 <kjdumba@gmail.com>
2019-11-16 21:28:12 +0300

    add landing page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import "./home.scss"
+import image from '../../Assets/images/logo.svg';
+import Navbar from '../common/NavBar';
 
 const Home =  ()=> {
       return (
         <React.Fragment>
             <div className="home">
-            <p>Welcome to WICS!</p>
+              <Navbar/>
+               <div className="center">
+                <img className="image" src={image} alt="logo"></img>
+                <p className ="subtitle">Wildlife Intelligent Crime Scanner</p>
+              </div>        
             </div>
         </React.Fragment>
       );
