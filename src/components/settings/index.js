23122665a6fee0ca47deca41a4b1002424fbbbae
kenneth051 <kjdumba@gmail.com>
2019-11-17 07:58:17 +0300

    add submit functionality

--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -1,45 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./settings.scss"
 import Navbar from '../common/NavBar';
+function sendSettings(e) {
+    e.preventdefault()
+    const url = document.getElementById()
 
-const Settings = () => {
-    return (
-        <React.Fragment>
-            <div className="settings">
-                <Navbar />
-                <p class="head">Settings</p>
-                <form>
-                    <div class="row">
-                        <div class="col-lg-3"></div>
-                        <div class="col-lg-3">
-                            <label for="address">Address</label>
-                            <input type="text" class="form-control" id="address" name="address" placeholder="Enter Address" /><br />
-                            <label for="scanInterval">Scan Interval</label>
-                            <select class="form-control" id="exampleFormControlSelect2">
-                                <option>Daily</option>
-                                <option>Weekly</option>
-                                <option>Monthly</option>
-                                <option>Anually</option>
-                            </select><br />
-                            <label for="Addkeywords">Add Key Words</label>
-                            <input type="text" class="form-control" id="keyWords" name="keyWords" placeholder="Add keyWords separated with a comma" /><br />
-                            <input type="submit" name="submit" class ="btn btn-success"/>
-                        </div>
+}
+class Settings extends Component {
+    state = {
+        "address": "",
+        "keyWords": ""
+    }
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+    sendKeyword = () => {
+        const data = { "word": this.state.keyWords };
+        fetch('http://0.0.0.0:5000/keyword', {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
 
-                        <div class="col-lg-3">
-                            <label for="exampleFormControlSelect2">Scan Interval</label>
-                            <select class="form-control" id="exampleFormControlSelect2">
-                                <option>English</option>
-                                <option>French</option>
-                                <option>Luganda</option>
-                            </select><br />
+        })
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+            .catch(err=>{
+                console.log(err)
+            })
+    };
+
+    sendAddress = () => {
+        const url = { "url": this.state.address }
+        fetch('http://0.0.0.0:5000/website', {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(url)
+
+        })
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+            .catch(err=>{
+                console.log("error")
+            })
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.keyWords) {
+            this.sendKeyword()
+        }
+        if(this.state.address){
+            this.sendAddress()
+        }
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="settings">
+                    <Navbar />
+                    <p className="head">Settings</p>
+                    <form method="POST" onSubmit={this.handleSubmit}>
+                        <div className="row">
+                            <div className="col-lg-3"></div>
+                            <div className="col-lg-3">
+                                <label for="address">Address</label>
+                                <input type="text" className="form-control" id="address" name="address" onChange={this.onChange} placeholder="Enter Address" /><br />
+                                <label for="scanInterval">Scan Interval</label>
+                                <select className="form-control" id="exampleFormControlSelect2">
+                                    <option>Daily</option>
+                                    <option>Weekly</option>
+                                    <option>Monthly</option>
+                                    <option>Anually</option>
+                                </select><br />
+                                <label for="Addkeywords">Add Key Words</label>
+                                <input type="text" className="form-control" id="keyWords" name="keyWords" onChange={this.onChange} placeholder="Add keyWords separated with a comma" /><br />
+                                <input type="submit" name="submit" class="btn btn-success" />
+                            </div>
+
+                            <div className="col-lg-3">
+                                <label for="exampleFormControlSelect2">Select Language</label>
+                                <select className="form-control" id="exampleFormControlSelect2">
+                                    <option>English</option>
+                                    <option>French</option>
+                                    <option>Luganda</option>
+                                </select><br />
+                            </div>
+                            <div className="col-lg-3 "></div>
                         </div>
-                        <div class="col-lg-3 "></div>
-                    </div>
-                </form>
-            </div>
-        </React.Fragment>
-    );
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Settings;
